b20eba4c1a61a3aa96d232496d7d5882af591d44
Andrew Pethoud <andrewpethoud@gmail.com>
2022-03-04 11:13:11 -0800

    feat(navigation): stubbed out home screen navigation

---
+++ src/components/AddEvent.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+
+export default function AddEvent() {
+  return (
+    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+      <Text>Add Event</Text>
+    </View>
+  );
+}
