342d66a0b82d5c30c0880fcfac046fc7a2ddcf48
Andrew Pethoud <andrewpethoud@gmail.com>
2022-03-04 12:07:41 -0800

    feat(navigation): swap out stack navigator for tab navigator

--- App.tsx
+++ App.tsx
@@ -1,20 +1,20 @@
 import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
-import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 import Home from './src/components/Home';
 import EventList from './src/components/EventList';
 import AddEvent from './src/components/AddEvent';
 import Dashboard from './src/components/Dashboard';
 
-const Stack = createNativeStackNavigator();
+const Tab = createBottomTabNavigator();
 
 const App = () => {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="Home">
-        <Stack.Screen name="Home" component={Home} />
-        <Stack.Screen name="EventList" component={EventList} />
-      </Stack.Navigator>
+      <Tab.Navigator initialRouteName="Home">
+        <Tab.Screen name="Home" component={Home} />
+        <Tab.Screen name="EventList" component={EventList} />
+      </Tab.Navigator>
     </NavigationContainer>
   );
 };
