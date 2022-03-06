64b056bc521f673da2aed3207e8c1d8cf6b0f6b9
Andrew Pethoud <andrewpethoud@gmail.com>
2022-03-06 14:55:31 -0800

    add basic TextInput

--- src/components/AddEvent.tsx
+++ src/components/AddEvent.tsx
@@ -1,10 +1,25 @@
-import React from 'react';
-import {View, Text} from 'react-native';
+import React, { useState } from 'react';
+import { View, Text, TextInput, Button, StyleSheet } from 'react-native';
 
 export default function AddEvent() {
+  const [name, setName] = useState('');
+
   return (
-    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
-      <Text>Add Event</Text>
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>test text</Text>
+      <TextInput value={name} onChangeText={setName} style={styles.input} />
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  input: {
+    height: 48,
+    width: 240,
+    backgroundColor: '#fff',
+    borderWidth: 1,
+    borderStyle: 'solid',
+    borderColor: '#ccc',
+    borderRadius: 8,
+  },
+});
