a8f126bd2c69bc9ca86ba6930d16d6645f0b95ca
Andrew Pethoud <andrewpethoud@gmail.com>
2022-03-06 15:14:49 -0800

    feat(event form): move styling to styled components

--- src/components/AddEvent.tsx
+++ src/components/AddEvent.tsx
@@ -1,33 +1,14 @@
 import React, { useState } from 'react';
 import { View, Text, TextInput, Button, StyleSheet } from 'react-native';
+import { StyledTextInput, StyledInputLabel } from './styles';
 
 export default function AddEvent() {
   const [name, setName] = useState('');
 
   return (
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text style={styles.inputLabel}>Event Name</Text>
-      <TextInput
-        value={name}
-        onChangeText={setName}
-        style={styles.inputField}
-      />
+      <StyledInputLabel>Event Name</StyledInputLabel>
+      <StyledTextInput value={name} onChangeText={setName} />
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  inputField: {
-    height: 48,
-    width: 240,
-    backgroundColor: '#fff',
-    borderWidth: 1,
-    borderStyle: 'solid',
-    borderColor: '#ccc',
-    borderRadius: 8,
-  },
-  inputLabel: {
-    fontSize: 18,
-    color: '#222',
-  },
-});
---
+++ src/components/styles.tsx
@@ -0,0 +1,15 @@
+import styled from 'styled-components/native';
+import { TextInput } from 'react-native';
+
+export const StyledTextInput = styled.TextInput`
+  height: 48px;
+  width: 240px;
+  background-color: '#fff';
+  border: 1px solid #ccc;
+  border-radius: 8px;
+`;
+
+export const StyledInputLabel = styled.Text`
+  font-size: 18px;
+  color: '#222';
+`;
