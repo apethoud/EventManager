e9c285758d3c60dd77a86a28adff45fde2479964
Andrew Pethoud <andrewpethoud@gmail.com>
2022-03-09 10:04:48 -0800

    feat(event form): wire up form

--- src/app/features/EventList.tsx
+++ src/app/features/EventList.tsx
@@ -1,10 +1,15 @@
 import React from 'react';
-import {View, Text} from 'react-native';
+import { View, Text } from 'react-native';
+import { RootState } from '../store';
+import { useSelector } from 'react-redux';
 
 export default function EventList() {
+  const eventList = useSelector((state: RootState) => state.events.eventList)
+
   return (
     <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
       <Text>Event List</Text>
+      <Text>Total events: {eventList.length}</Text>
     </View>
   );
 }
